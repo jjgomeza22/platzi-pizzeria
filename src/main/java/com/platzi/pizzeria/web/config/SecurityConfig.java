c7bfe982bdc35478c75d768ac3f0c2505bfd3767
juan_gomez9905 <juan.gomez@bebolder.co>
2024-04-17 22:05:07 -0500

    adding an user in memory

--- src/main/java/com/platzi/pizzeria/web/config/SecurityConfig.java
+++ src/main/java/com/platzi/pizzeria/web/config/SecurityConfig.java
@@ -6,6 +6,12 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -14,7 +20,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http.authorizeHttpRequests(customRequest -> customRequest
-                        .requestMatchers(HttpMethod.GET,"/api/**").permitAll() //with **, permit all the GET request
+                        .requestMatchers(HttpMethod.GET, "/api/pizzas/**").permitAll() //with **, permit all the GET request
                         .requestMatchers(HttpMethod.PUT).denyAll() //It does not have patter, it applies to all project
                         .anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults())
@@ -22,5 +28,21 @@ public class SecurityConfig {
                 .cors(Customizer.withDefaults());
 
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService memoryUsers() {
+        UserDetails adminUser = User.builder()
+                .username("admin")
+                .password(passwordEncoder().encode("admin"))
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(adminUser);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
