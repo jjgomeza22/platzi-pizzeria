9794db6c6f6cd747cb20380294bc24c695de8c5d
juan_gomez9905 <juan.gomez@bebolder.co>
2024-04-17 21:44:37 -0500

    add requestMatchers to allow some request with some rules

--- src/main/java/com/platzi/pizzeria/web/config/SecurityConfig.java
+++ src/main/java/com/platzi/pizzeria/web/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.platzi.pizzeria.web.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -11,14 +12,14 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
+
+        http.authorizeHttpRequests(customRequest -> customRequest
+                        .requestMatchers(HttpMethod.GET,"/api/**").permitAll() //with **, permit all the GET request
+                        .requestMatchers(HttpMethod.PUT).denyAll() //It does not have patter, it applies to all project
+                        .anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
-                .cors(Customizer.withDefaults())
-                .authorizeHttpRequests(customizeRequests ->
-                        customizeRequests
-                                .anyRequest()
-                                .authenticated())
-                .httpBasic(Customizer.withDefaults());
+                .cors(Customizer.withDefaults());
 
         return http.build();
     }
