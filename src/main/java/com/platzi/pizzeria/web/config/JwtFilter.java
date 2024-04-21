22a7283dba542d46eee0ca1e22e98b64543af586
juan_gomez9905 <juan.gomez@bebolder.co>
2024-04-20 22:18:51 -0500

    adding details to the authenticationToke to see the ip and session

--- src/main/java/com/platzi/pizzeria/web/config/JwtFilter.java
+++ src/main/java/com/platzi/pizzeria/web/config/JwtFilter.java
@@ -10,6 +10,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -53,6 +54,7 @@ public class JwtFilter extends OncePerRequestFilter {
                 user.getPassword(),
                 user.getAuthorities()
         );
+        authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
         SecurityContextHolder.getContext().setAuthentication(authenticationToken);
         System.out.println(authenticationToken);
         filterChain.doFilter(request, response);
