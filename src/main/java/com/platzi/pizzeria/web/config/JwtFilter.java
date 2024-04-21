1b2a09a9d5b87b64df9941b1d9d7afd8a35a56bd
juan_gomez9905 <juan.gomez@bebolder.co>
2024-04-20 22:02:45 -0500

    adding a filter to verify if JWT is correct to after add the filter to the filterChain

---
+++ src/main/java/com/platzi/pizzeria/web/config/JwtFilter.java
@@ -0,0 +1,59 @@
+package com.platzi.pizzeria.web.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtFilter extends OncePerRequestFilter {
+    private final JwtUtils jwtUtils;
+    private final UserDetailsService userDetailsService;
+
+    @Autowired
+    public JwtFilter(JwtUtils jwtUtils, UserDetailsService userDetailsService) {
+        this.jwtUtils = jwtUtils;
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request, HttpServletResponse response, FilterChain filterChain
+    ) throws ServletException, IOException {
+        //Steps to validate a JWT token
+        //1. To validate that it's being a valid Authorization header
+        String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+        if (authHeader == null || authHeader.isEmpty() || !authHeader.startsWith("Bearer")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        //2. To validate if JWT is valid
+        String jwt = authHeader.split(" ")[1].trim();
+        if (!this.jwtUtils.isValid(jwt)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        //3. Load user from UserDetailService
+        String username = this.jwtUtils.getUserName(jwt);
+        User user = (User) this.userDetailsService.loadUserByUsername(username);
+
+        //4. Load user in the security context
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                user.getUsername(),
+                user.getPassword(),
+                user.getAuthorities()
+        );
+        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+        filterChain.doFilter(request, response);
+    }
+}
