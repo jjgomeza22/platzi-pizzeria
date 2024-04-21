4a5b6588a1fd10242a9194275353c3819435813a
juan_gomez9905 <juan.gomez@bebolder.co>
2024-04-20 21:37:42 -0500

    implement a validation method for JWT

--- src/main/java/com/platzi/pizzeria/web/config/JwtUtils.java
+++ src/main/java/com/platzi/pizzeria/web/config/JwtUtils.java
@@ -2,6 +2,7 @@ package com.platzi.pizzeria.web.config;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
@@ -11,6 +12,7 @@ import java.util.concurrent.TimeUnit;
 public class JwtUtils {
     private static String SECRET_KEY = "pl4tz1_p1zz4";
     private static Algorithm ALGORITHM = Algorithm.HMAC256(SECRET_KEY);
+
     public String create(String username) {
         return JWT.create()
                 .withSubject(username)
@@ -18,5 +20,23 @@ public class JwtUtils {
                 .withIssuedAt(new Date())
                 .withExpiresAt(new Date(System.currentTimeMillis() + TimeUnit.DAYS.toMillis(15)))
                 .sign(ALGORITHM);
+    }
+
+    public boolean isValid(String jwt) {
+        try {
+            JWT.require(ALGORITHM)
+                    .build()
+                    .verify(jwt);
+            return true;
+        } catch (JWTVerificationException e) {
+            return false;
+        }
+    }
+
+    public String getUserName(String jwt) {
+        return JWT.require(ALGORITHM)
+                .build()
+                .verify(jwt)
+                .getSubject();
     }
 }
